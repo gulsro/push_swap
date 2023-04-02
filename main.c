20aaca5526ccb2a2339cab592ca675944fecc6f3
gulser <glserozkan@gmail.com>
2023-04-02 14:08:36 +0200

    normz

--- main.c
+++ main.c
@@ -1,31 +1,43 @@
 #include "push_swap.h"
 
+static  void    decide_sorting(stack *a, stack *b, int argc_number)
+{
+        if (argc_number == 2)
+                sort_2(a);
+        else if (argc_number == 3)
+                sort_3(a);
+        else if (argc_number == 4)
+                sort_4(a, b, biggest_num(a));
+        else if (argc_number == 5)
+                sort_5(a, b, biggest_num(a), smallest_num(a));
+        else
+                large_sort(a, b);
+}
+
 int main(int argc, char *argv[])
 {
-	stack *a;
-	stack *b;
-	
+	stack	*a;
+	stack	*b;
+	int		i;
+
+	if (argc < 2)
+		error_exit();
 	a = initialize_stack();
 	b = initialize_stack();
-	if (argc < 2)
-		exit(1);
+	if (!a || !b)
+		error_exit();
 	if (check_all_digit(argv) != 1)
 		error_exit();
-	for (int i = 1; i < argc; i++)
+	i = 1;
+	while (i < argc)
+	{
 		initial_stack_elem(a, ft_atoi(argv[i]));
+		i++;
+	}
 	if (check_repeat(a) != 1 || check_sorted(a) == 1)
 		error_exit();
 	assign_index(a);
-	if (argc == 2)
-		sort_2(a);
-	else if (argc == 3)
-		sort_3(a);
-	else if (argc == 4)
-		sort_4(a, b, biggest_num(a));
-	else if (argc == 5)
-		sort_5(a, b, biggest_num(a), smallest_num(a));
-	else
-		large_sort(a, b);
+	decide_sorting(a, b, argc);
 	free_stack(a);
 	free_stack(b);
 //	system("leaks another");
